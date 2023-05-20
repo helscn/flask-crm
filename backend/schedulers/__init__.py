083601b4bb31e8b2db3ac32a51a8ea73e32d5c19
helscn <helscns@gmail.com>
2023-05-20 17:16:22 +0800

    +增加Schedule任务的文件锁支持，避免任务重复运行

--- backend/schedulers/__init__.py
+++ backend/schedulers/__init__.py
@@ -8,22 +8,22 @@ from main import scheduler
 from flask import Blueprint
 from flask_restful import Api
 
-# from .filelock import FileLock
 
 __all__ = ['Scheduler', 'SchedulerLock', 'scheduler']
 
 # 获取文件锁
-# BASE_DIR = path.abspath(path.dirname(__file__))
-# SchedulerLock = FileLock(path.join(BASE_DIR, 'scheduler.lock'))
-# SchedulerLock.lock()
+from .filelock import FileLock
+BASE_DIR = path.abspath(path.dirname(__file__))
+SchedulerLock = FileLock(path.join(BASE_DIR, 'scheduler.lock'))
+SchedulerLock.lock()
 
 # 创建 scheduler 资源蓝图
 Scheduler = Blueprint('scheduler', __name__)
 
 # 如果获取到文件锁，将导入的 Restful API 资源注册到蓝图中，并启动 scheduler
-# if SchedulerLock.locked:
-api = Api(Scheduler)
-api.add_resource(ApiJobs, '/jobs', endpoint='jobs')
-api.add_resource(ApiJob, '/job/<id>', endpoint='job')
-api.add_resource(ApiLogs, '/logs', endpoint='logs')
-scheduler.start()
+if SchedulerLock.locked:
+    api = Api(Scheduler)
+    api.add_resource(ApiJobs, '/jobs', endpoint='jobs')
+    api.add_resource(ApiJob, '/job/<id>', endpoint='job')
+    api.add_resource(ApiLogs, '/logs', endpoint='logs')
+    scheduler.start()
